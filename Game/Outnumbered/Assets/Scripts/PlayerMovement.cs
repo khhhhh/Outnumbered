f392d1c56b2ad8f932006a5786c8bb1908408bb3
khhhhh <oleg.hutsko@gmail.com>
2021-01-21 19:31:19 +0100

    change character, adding some terrain and zombies

---
+++ Game/Outnumbered/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
