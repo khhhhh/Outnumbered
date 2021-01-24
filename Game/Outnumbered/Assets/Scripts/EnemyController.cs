7d25d3ede81cf0dc0d6790d2007feb6f30c48447
khhhhh <oleg.hutsko@gmail.com>
2021-01-24 16:14:11 +0100

    adding enemies

---
+++ Game/Outnumbered/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody myRB;
+    public float moveSpeed;
+
+    public Invector.vCharacterController.vThirdPersonController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        player = FindObjectOfType<Invector.vCharacterController.vThirdPersonController>();
+    }
+    void FixedUpdate()
+    {
+        myRB.velocity = (transform.forward * moveSpeed);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+    }
+}
---
+++ Game/Outnumbered/Assets/Scripts/GunController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public bool isFiring;
+
+    public BulletControl bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                BulletControl newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletControl;
+                newBullet.speed = bulletSpeed;
+            }
+        }
+        else
+        {
+            shotCounter = 0;
+        }
+    }
+}
